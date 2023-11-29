ceb64fca7ddb2e6babaddd85faf2b08378e64577
Guillaume <Guillaume.demicheli@gmail.com>
2023-11-29 01:17:28 +0100

    clean

--- Wifi.Linux.Sample.Console.C/main.cpp
+++ Wifi.Linux.Sample.Console.C/main.cpp
@@ -4,20 +4,12 @@
 #include "../Wifi.Linux.Core/wrapper.h"
 #include "../Wifi.Linux.Core/iwconfig.c"
 
-typedef struct {
-
-	int a;
-	int b;
-	int c;
-} my_struct;
-
-
 int main(void) {
 	wireless_scan_head head;
 	wireless_scan* result;
 	iwrange range;
 	int sock;
-	SCAN* result2;
+	SCAN* scanResult;
 
 
 	/* Open socket to kernel */
@@ -35,22 +27,7 @@ int main(void) {
 		exit(2);
 	}
 
-
-	/*my_struct* first;
-	first->a = 100; first->b = 101; first->c = 1000;
-
-	my_struct* bb;
-
-	printf("first %d %d %d\n", first->a, first->b, first->c);
-	bb = first;
-	printf("second %d %d %d\n", bb->a, first->b, bb->c);
-
-
-	first->a = 55; first->b = 55; first->c = 89;
-	printf("second %d %d %d\n", bb->a, first->b, bb->c);*/
-
-
-	//int t = wrapper(result2);
+	//int t = wrapper(scanResult);
 	SCAN localScan;
 
 	int i = 0;
@@ -59,47 +36,28 @@ int main(void) {
 	result = head.result;
 	while (NULL != result) {
 		printf("%s\n", result->b.essid);
-		
-		/*	WIFI wifi;
-			strcpy(wifi.essid, result->b.essid);*/
-
-			//*result2->scans[i].b = result->b;
-			//result2->scans[i] = *result;
-
-		/*	localScan.wifis[i] = wifi;
-			localScan.scans[i] = result;*/
-
-			//WIFI wifi;
-			//strcpy(wifi.essid, result->b.essid);
-			//localScan.wifis[i] = wifi;
 		localScan.scans[i] = result;
-
-		//memcpy(result2->scans[i], result, sizeof(result));
 		i++;
-		//result2->scans[i].b = result->b;
 		result = result->next;
 	}
 
 	SCAN* pointerScan = &localScan;
 
-	result2 = &localScan;
+	scanResult = &localScan;
 
-	SCAN ss = *result2;
+	SCAN ss = *scanResult;
 
 	for (int j = 0; j < i; j++)
 	{
-		//printf("wifis %s\n", *result2->wifis[j]->essid);
-		//printf("scans %s\n", *result2->scans[j]->b.essid);
-		//printf("ESSID: " + localScan->wifis[j].essid);
-		//printf("ESSID: " + localScan.scans[j]->b->essid);
 		printf("ESSID: %s", ss.scans[j]->b.essid);
 	}
 
+	// WIP
 	//iwconfig_cmd cm = *find_command("essid GDM key _0123456789!");
 	//char* essid = ss.scans[0]->b.essid;
-	char* essid = "iPhone de Guillaume";
+	/*char* essid = "iPhone de Guillaume";
 
-	set_essid_info(sock, "wlan0", &essid, 1);
+	set_essid_info(sock, "wlan0", &essid, 1);*/
 
 	exit(0);
 }
